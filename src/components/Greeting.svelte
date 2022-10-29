214d190489911d10f6c2b5e96c1536a40486610c
Tanner Hansen <tanner.hansen.212@gmail.com>
2022-10-29 15:45:20 -0600

    Finish tutorial

---
+++ src/components/Greeting.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    export let messages: string[];
+    
+    let greeting: string;
+
+    function randomMessage() {
+        greeting = messages[Math.floor(Math.random() * messages.length)];
+    }
+
+    randomMessage();
+</script>
+
+<div>
+    <h3>{greeting}!</h3>
+    <button on:click={randomMessage}>
+        New Greeting
+    </button>
+</div>
